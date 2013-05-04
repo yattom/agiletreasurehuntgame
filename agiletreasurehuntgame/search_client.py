823750b725193ee53720f8a2da718ff131f1b52d
yattom <tsutomu.yasui@gmail.com>
2013-05-04 17:41:18 +0900

    split server and client

---
+++ agiletreasurehuntgame/search_client.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+import urllib2
+
+from search_server import encode, decode
+
+class SearchClient(object):
+    def __init__(self, url):
+        self.url = url
+
+    def search(self):
+        for candidate in self.candidates():
+            self.process_candidate(candidate)
+
+    def process_candidate(self, candidate):
+        if not candidate.is_final():
+            self.add_candiates(candidate.next_states())
+        self.add_processed(candidate)
+
+    def candidates(self):
+        while True:
+            f = urllib2.urlopen(self.url + '/candidates')
+            candidate = decode(f.read())
+            print candidate
+            if not candidate: raise StopIteration
+            yield candidate
+
+    def add_processed(self, processed):
+        f = urllib2.urlopen(self.url + '/processed', data=encode(processed))
+        f.close()
+
+    def add_candiates(self, next_candidates):
+        data = encode(list(next_candidates))
+        f = urllib2.urlopen(self.url + '/candidates', data=data)
+        f.close()
+
+
--- agiletreasurehuntgame/search_server.py
+++ agiletreasurehuntgame/search_server.py
@@ -4,7 +4,6 @@ import web
 from web.webapi import context
 import pickle
 import base64
-import urllib2
 
 from search import Search
 
@@ -51,35 +50,6 @@ class SearchServer(object):
         app = web.application(SearchServer.urls, cls.__dict__, autoreload=True)
         app.run()
 
-class SearchClient(object):
-    def __init__(self, url):
-        self.url = url
-
-    def search(self):
-        for candidate in self.candidates():
-            self.process_candidate(candidate)
-
-    def process_candidate(self, candidate):
-        if not candidate.is_final():
-            self.add_candiates(candidate.next_states())
-        self.add_processed(candidate)
-
-    def candidates(self):
-        while True:
-            f = urllib2.urlopen(self.url + '/candidates')
-            candidate = decode(f.read())
-            print candidate
-            if not candidate: raise StopIteration
-            yield candidate
-
-    def add_processed(self, processed):
-        f = urllib2.urlopen(self.url + '/processed', data=encode(processed))
-        f.close()
-
-    def add_candiates(self, next_candidates):
-        data = encode(list(next_candidates))
-        f = urllib2.urlopen(self.url + '/candidates', data=data)
-        f.close()
 
 def main():
     import othello, search
